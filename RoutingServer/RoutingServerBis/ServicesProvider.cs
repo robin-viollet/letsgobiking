1350ef51e15dde8407c5d4de73fcebda6b9b3caf
Thomas Guiot <thomas.guiot@etu.univ-cotedazur.fr>
2022-12-06 23:41:16 +0100

    Cleared the queue before posting instructions

--- RoutingServer/RoutingServerBis/ServicesProvider.cs
+++ RoutingServer/RoutingServerBis/ServicesProvider.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Globalization;
@@ -174,6 +174,8 @@ namespace RoutingServer
         {
             // Use the session to target a queue.
             IDestination destination = session.GetQueue("" + (++queueId));
+
+            session.DeleteDestination(destination);
 
             // Create a Producer targetting the selected queue.
             IMessageProducer producer = session.CreateProducer(destination);
